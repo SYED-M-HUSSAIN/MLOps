ee7d4c4d4630a2f2eed1626c96c2360f90533f9a
Muhammad Hussain <88007126+SYED-M-HUSSAIN@users.noreply.github.com>
2025-05-05 01:35:24 +0500

    Revert "integrated the whole repo into single platform"

--- scripts/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Constants
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
-ANTHROPIC_API_KEY = os.getenv('ANTHROPIC_API_KEY')
-REPO_OWNER = os.getenv('REPO_OWNER')
-REPO_NAME = os.getenv('REPO_NAME')
-PR_NUMBER = int(os.getenv('PR_NUMBER'))
